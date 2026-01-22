0c20a1bce452ae3385d45837997a318955e0f3cf
Apostolovv777 <n.apostolov2021@pmg-pernik.com>
2026-01-22 15:28:28 +0200

    Adding models

---
+++ AutoPartsShop.Infrastructure/Data/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPartsShop.Infrastructure.Data.Entities
+{
+    public class ApplicationUser:IdentityUser
+    {
+    }
+}
