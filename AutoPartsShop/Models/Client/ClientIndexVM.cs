0378c26f47394f68e8b3f5fc7443f2ed21145c05
unknown <n.apostolov2021@pmg-pernik.com>
2026-03-15 20:51:11 +0200

    part66

--- AutoPartsShop/Models/Client/ClientIndexVM.cs
+++ AutoPartsShop/Models/Client/ClientIndexVM.cs
@@ -1,6 +1,24 @@
-﻿namespace AutoPartsShop.Models.Client
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPartsShop.Models.Client
 {
     public class ClientIndexVM
     {
+        public string Id { get; set; } = null!;
+
+        [Display(Name = "Username")]
+        public string UserName { get; set; } = null!;
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = null!;
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public bool IsAdmin { get; set; }
     }
 }
